efc95e4a2387df38ffef9aa5c660b86cc9d9b7dc
MasonMello <masonmello2006@gmail.com>
2026-04-07 11:45:08 -0400

    Prepare backend for deployment

--- server/index.js
+++ server/index.js
@@ -56,6 +56,7 @@ app.post("/calculate", (req, res) => {
   res.json({ riskScore, premium, ageRisk, accidentRisk, carTypeRisk });
 });
 
-app.listen(3001, () => {
-  console.log("Server running on port 3001");
+const PORT = process.env.PORT || 3001; 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
